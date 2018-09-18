e93b0c1c4c66ef09004de39c033b8bb940856e17
lukewhyte <lukeawhyte@gmail.com>
2018-09-17 22:56:03 -0500

    Finished filtering school list, helmets through 5A

--- src/reducers/utils.js
+++ src/reducers/utils.js
@@ -5,17 +5,19 @@ export const buildSAMap = days => {
     const saMap = {};
     days.forEach(day => {
         day.games.forEach(game => {
-            if (!saMap[game.home.id] && game.home.district && game.home.district.indexOf('6A') !== -1 && game.home.district.indexOf('28') !== -1) {
+            if (!saMap[game.home.id] && game.home.district && game.home.district.indexOf('6A') !== -1 && game.home.district.indexOf('26') !== -1) {
                 console.log(game.home.id, game.home.district, game.home.name);
                 saMap[game.home.id] = game.home.name;
             }
 
-            if (!saMap[game.away.id] && game.away.district && game.away.district.indexOf('6A') !== -1 && game.home.district.indexOf('28') !== -1) {
+            if (!saMap[game.away.id] && game.away.district && game.away.district.indexOf('6A') !== -1 && game.away.district.indexOf('26') !== -1) {
                 console.log(game.away.id, game.away.district, game.away.name);
                 saMap[game.away.id] = game.away.name;
             }
         });
     });
 };
+
+// Remove check for 6B etc!!!!
 
 export const parseGames = games => sortData(games.reduce(mapData, {}));
